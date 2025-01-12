2f1db4dc711226e942c1211cb043460fb3a8553f
huzerhan <104505662+huzerhan@users.noreply.github.com>
2025-01-12 22:20:48 +0300

    Resolved conflict

---
+++ theme/src/pages/dashboards/IndexExperience.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex w-full justify-between h-screen">
+    <InputPanel></InputPanel>
+    <TargetPreview></TargetPreview>
+  </div>
+</template>
+
+<script setup>
+import TargetPreview from "../../IframeTarget/components/TargetPreview.vue";
+import InputPanel from "../../IframeTarget/components/InputPanel/InputPanel.vue";
+</script>
+
+<style></style>
